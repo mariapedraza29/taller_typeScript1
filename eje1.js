dc4376cd1d81091720f9fc1eba7dd229f5329efb
mariapedraza29 <pedrazamariia@gmaul.com>
2023-06-28 17:42:04 -0500

    ejercicios de typeScript1

---
+++ eje1.js
@@ -0,0 +1,79 @@
+//let varlog1=true;
+//let varlog2=false;
+/*let edaad:number= 18;
+let estatura:number=231;
+
+let subsidio:any=32;
+let nombreCarro: any ="chevrolet";
+
+let nomb:string= "Juan";
+let apell:string="Mayorga";
+
+let msj= `Bienvenido  ${nomb} ${apell} a nuestro sitio web, gracias por registrarte`
+
+console.log(msj);
+
+
+let cadena:number[];
+cadena=[-15,12,14,83,23];
+
+let arrayStrings: string[];
+arrayStrings=["holii", "soy","Maria","tengo 18, ", "estudio en el SENA"];
+
+for(let j=0; j<cadena.length;j++){
+    console.log(cadena[j]);
+    
+}
+for(let i=0; i<arrayStrings.length;i++){
+    console.log(arrayStrings[i]);
+}
+cadena.pop();
+console.log(cadena);
+
+cadena.shift();
+console.log(cadena);
+
+cadena.push(32);
+console.log(cadena);
+
+cadena.unshift(42);
+console.log(cadena);
+
+const osArray = [
+    {id: 0, name: "Windows"},
+    {id: 1, name: "Linux"},
+    {id: 2, name: "MacOS"},
+];
+    
+const updatedOSArray = osArray.map(p =>
+  p.id === 1
+    ? { name: 'Ubuntu' } : p
+);
+console.log(updatedOSArray);
+*/
+const diasSem = [
+    { dia: "lunes" },
+    { dia: "martes" },
+    { dia: "miercoles" },
+    { dia: "jueves" },
+    { dia: "viernes" },
+    { dia: "sabado" },
+    { dia: "domingo" }
+];
+const numeros = [
+    { num1: 1 },
+    { num1: 2 },
+    { num1: 3 },
+    { num1: 4 },
+    { num1: 5 },
+    { num1: 6 },
+    { num1: 7 },
+    { num1: 8 },
+    { num1: 9 }
+];
+for (let index = 0; index < diasSem.length; index++) {
+}
+console.log(diasSem);
+for (let index = 0; index < diasSem.length; index++) {
+}
+console.log(numeros);
---
+++ index.ts
@@ -0,0 +1,70 @@
+//tipo de dato numerico
+let edad:number = 20;
+//tipo boolean
+let estado:boolean = false;
+//tipo string
+let nombre: string = "ADSO 321";
+let mensaje = `Hola   ${nombre} tu edad es ${edad}`;
+console.log(mensaje);
+//tipo any(la usamos solo si se desconoce el tipo de dato que se guardara)
+let peso: any = 30;
+//condicional
+if (peso >= 30){
+  console.log("Ud pesa por lo menos 30");
+}else{
+  console.log("Ud pesa menos de 30");
+}
+peso = "treinta";
+//arreglo de solo numeros
+let arregloN: number[] = [2, 8, 9];
+//arreglos
+let arregloMixto:any = [true, "HOLA", 15, 10.7];
+//lectura
+console.log("-----", arregloMixto[1]);
+//asignacion
+arregloMixto[2] = 30;
+//eliminacion
+//aca eliminamos el elemnto de indice 0, y a partir
+// de este solo eliminamos un elemento, es decir, él mismo
+arregloMixto.splice(0, 1);
+//agregacion
+arregloMixto.push(400);
+//copia independiente
+let copiaArreglo = arregloMixto.slice();
+console.log("arreglo copia sin modificar", copiaArreglo);
+arregloMixto.splice(1, 1);
+console.log("arreglo copia modificado el original", copiaArreglo);
+//recorrido con forEach
+console.log("----------------__");
+arregloMixto.forEach((elemento: any) => {
+  console.log(elemento);
+})
+//Objetos (llave-valor)
+let diasSemana = {lunes: "Lunes", martes: "Martes", miercoles: "Miercoles", 2: "dos"};
+//lectura
+console.log(diasSemana.martes);
+console.log(diasSemana["miercoles"]);
+console.log(diasSemana[2]);
+//escritura
+diasSemana["lunes"] = "LUNES";
+//eliminacion
+delete diasSemana["lunes"];
+console.log(diasSemana);
+//asignacion
+diasSemana["sabado"] = "Sabado";
+//recorrido
+console.log("----------+++++++");
+for (const llave in diasSemana) {
+    console.log(llave);
+    console.log(diasSemana[llave]);
+    console.log("-----------------");
+}
+
+
+
+
+
+
+
+
+
